f273ddc46df8669a32ddbf6dacb5c7e81e7ff41f
nottak0409 <nottak0409@gmail.com>
2021-06-08 07:52:28 +0900

    change

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //モジュールインポート
 const server = require("express")();
-const line = require("@line/bot-sdk");
+const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
 //パラメーター設定（環境変数を利用）
 const line_config = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN,
-    channelSecret: process.env.LINE_CHANNEL_SECRET
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
+    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
 };
 
 //Webサーバー設定
@@ -14,4 +14,5 @@ server.listen(process.env.PORT || 3000);
 //ルーターの設定
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     res.sendStatus(200);
-})
+    console.log(req.body);
+});
